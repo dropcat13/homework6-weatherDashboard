0db9c0796631dc1a2269dbb6f0515e0fff2b12ce
Cat Kretzschmar <cat.ktz@gmail.com>
2020-01-02 11:55:05 -0500

    some of five day forecast working

--- script.js
+++ script.js
@@ -1,30 +1,39 @@
 // save place to local storage and append to the table in the card.
 var pastSearches = [];
 var apiData;
-var uvqueryURL; 
+var uvqueryURL;
 var latVar;
 var lonVar;
 var x;
+var previousSearches;
+var APIKey = "92ce17e1323ad237fdf4a085b335d5bf";
+var fivedayqueryURL;
 
-var storedCities = localStorage.getItem("weatherCities"); 
-console.log(storedCities); 
-if (storedCities === null) { 
-    weatherCities = ["Raleigh"];      
+renderButtons();
+
+var storedCities = localStorage.getItem("weatherCities");
+console.log(storedCities);
+if (storedCities === null) {
+    weatherCities = ["Raleigh"];
 }
-else { 
-    weatherCities = JSON.parse(storedCities); 
-    lastCityIndex = localStorage.getItem("lastCityIndex"); 
+else {
+    weatherCities = JSON.parse(storedCities);
+    lastCityIndex = localStorage.getItem("lastCityIndex");
 }
 
 function renderButtons() {
-    $("#pastLocations").empty();
-
-    for (var i = 0; i < pastSearches.length; i++) {
-        var a = $("<button>");
-        a.addClass("pastSearches");
-        a.attr("data-name", pastSearches[i]);
-        a.text(pastSearches[i]);
-        $("#pastLocations").append(a);
+    $("#pastLocations").empty(); 
+        previousSearches = localStorage.getItem("previous").split(",") 
+    if (previousSearches) {
+        previousSearches = localStorage.getItem("previous").split(",") 
+        console.log("previous searches ", previousSearches);
+        for (var i = 0; i < previousSearches.length; i++) {
+            var a = $("<button>");
+            a.addClass("pastSearches");
+            a.attr("data-name", previousSearches[i]);
+            a.text(previousSearches[i]);
+            $("#pastLocations").append(a);
+        }
     }
 }
 
@@ -32,27 +41,34 @@ function renderButtons() {
 
 $("#searchButton").on("click", function (event) {
     setCurrentInfo();
-    renderButtons();
+    setFiveDayForecast();
     $("#data").empty();
     $("#search-input").empty();
 });
 
-$(".pastSearches").on("click", function (event){ 
-    setPastInfo();
-})
+// $(document).on("click", ".pastSearches", function (event) {
+//     setCurrentInfo();
+// })
 
 $(document).on("click", ".pastSearches", function () {
     setPastInfo($(this).text());
+    // setFiveDayForecast();
 });
 
 function setCurrentInfo() {
     event.preventDefault();
 
-    var APIKey = localStorage.getItem("APIKey");
-    var MapAPIKey = "AIzaSyBhZv5uJpsqNdH9Eq0MGHrvYAPeVYSpHcQ";
+    // var APIKey = localStorage.getItem("APIKey");
 
     var userSearchLocation = $("#search-input").val().trim();
-    pastSearches.push(userSearchLocation);
+    previousSearches = localStorage.getItem("previous");
+    if (previousSearches) {
+        previousSearches += ',' + userSearchLocation
+    } else {
+        previousSearches = userSearchLocation
+    }
+
+    localStorage.setItem("previous", previousSearches)
 
     console.log(userSearchLocation);
 
@@ -75,31 +91,31 @@ function setCurrentInfo() {
         $("#icon").html(iconSrc)
         $("#data").html(`<div><h4>Temperature: ${response.main.temp}ºF</h4></div>
             <h4>Humidity: ${response.main.humidity}%</h4>
-            <h4>Wind: ${response.wind.speed}mph</h4>`)  
-  
-        var uvURL = "https://api.openweathermap.org/data/2.5/uvi?lat=" + 
-        response.coord.lat + 
-        "&lon=" +  response.coord.lon + "&appid=" + APIKey;
+            <h4>Wind: ${response.wind.speed}mph</h4>`)
+
+        var uvURL = "https://api.openweathermap.org/data/2.5/uvi?lat=" +
+            response.coord.lat +
+            "&lon=" + response.coord.lon + "&appid=" + APIKey;
 
         $.ajax({
             url: uvURL,
-            method: "GET" 
-        }).then(function (response) { 
+            method: "GET"
+        }).then(function (response) {
             var uvValue = response.value;
-            var uvDisplay = $("#uvIndex");  
-            uvDisplay.text("UV Index: " + uvValue); 
-            uvValue = parseFloat(uvValue); 
+            var uvDisplay = $("#uvIndex");
+            uvDisplay.text("UV Index: " + uvValue);
+            uvValue = parseFloat(uvValue);
             if (uvValue < 3) {
-                $("#uvIndex").attr("class","p-1 bg-success text-white"); 
+                $("#uvIndex").attr("class", "p-1 bg-success text-white");
             }
             else if (uvValue < 8) {
-                    $("#uvIndex").attr("class","p-1 bg-warning text-dark"); 
-                }
-                else if (uvValue >= 8) { 
-                    $("#uvIndex").attr("class","p-1 bg-danger text-white"); 
-                }
+                $("#uvIndex").attr("class", "p-1 bg-warning text-dark");
+            }
+            else if (uvValue >= 8) {
+                $("#uvIndex").attr("class", "p-1 bg-danger text-white");
+            }
         })
-    
+
         // convert to farenheit
         // var tempF = (response.main.temp - 273.15) * 1.80 + 32;
         //     $(".tempF").text("Temperature (Kelvin) " + tempF);
@@ -109,7 +125,7 @@ function setCurrentInfo() {
 function setPastInfo() {
     event.preventDefault();
 
-    var APIKey = localStorage.getItem("APIKey");
+    // var APIKey = localStorage.getItem("APIKey");
 
     var buttonText = $('.pastSearches').text();
     console.log(userSearchLocation);
@@ -121,9 +137,7 @@ function setPastInfo() {
         method: "GET"
     }).then(function (response) {
         apiData = response;
-        // Create CODE HERE to Log the queryURL
         console.log(queryURL);
-        // Create CODE HERE to log the resulting object
         console.log(response);
 
         $("#current").html(`<h1>${response.name}</h1>`)
@@ -133,27 +147,24 @@ function setPastInfo() {
         $("#data").append(`<div><h4>Temperature: ${response.main.temp}ºF</h4></div>
             <h4>Humidity: ${response.main.humidity}%</h4>
             <h4>Wind: ${response.wind.speed}mph</h4>
-            <h4>UV Index: ${response.main.visibility}</h4>`)  
-    
+            <h4>UV Index: ${response.main.visibility}</h4>`)
+
         // convert to farenheit
         // var tempF = (response.main.temp - 273.15) * 1.80 + 32;
         //     $(".tempF").text("Temperature (Kelvin) " + tempF);
     });
 }
 
+function setFiveDayForecast() {
+   // to only get one piece of data per day
+//    var APIKey = localStorage.getItem("APIKey");
 
-// $("#city").on("change keyup", function() {
-//     var city = $(this).val()
-//     $.getJSON("https://maps.googleapis.com/maps/api/geocode/json?address="+encodeURIComponent(city), function(val) {
-//       if(val.results.length) {
-//         var location = val.results[0].geometry.location
-//         $("#lat").val(location.lat)
-//         $("#lon").val(location.lng)
-//       }
-//     })
-//   })
-
-// /* <span><img id="weather-icon" src="https://openweathermap.org/img/wn/02d@2x.png"></span> */
-
-// to only get one piece of data per day:
-// if (data.list[i].dt_txt.indexOf("15:00:00") !== -1) {
+   var userSearchLocation = $("#search-input").val().trim();
+   fivedayqueryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + userSearchLocation + "&units=imperial&appid=" + APIKey;
+   console.log(response);
+//    if (data.list[i].dt_txt.indexOf("15:00:00") !== -1) {
+            $('#forecastData').append(`<div><h5>Temp: ${response.main.temp}ºF</h5></div>
+            <h5>Humidity: ${response.main.humidity}%<h5>`)
+            $('#forecastIcon').append(response.weather.id)
+//    }
+};
